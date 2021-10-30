d51cc3a151924accc8e948fa6ad05ad8cc9d6bad
melchilio <lioneltchami@gmail.com>
2021-10-30 17:47:33 -0600

    Changes on README.md

--- linux_monitor.sh
+++ linux_monitor.sh
@@ -1,6 +1,6 @@
                   ####################################################################################################
                   #                                        linux_monitor.sh                                        #
-                  # Written for Tecmint.com for the post www.tecmint.com/linux-server-health-monitoring-script/      #
+                  # Written for the post www.tecmint.com/linux-server-health-monitoring-script/      #
                   # If any bug, report us in the link below                                                          #
                   # Free to use/edit/distribute the code below by                                                    #
                   # giving proper credit to Tecmint.com and Author                                                   #
